b64c82014b47de0c3dc4751db47bd747df13c763
wenqin.xu <xuwenqin199175@aliyun.com>
2019-04-05 18:33:17 +0800

    20190405-update the test case and example GIFs

--- ascii_image/img_process/test.py
+++ ascii_image/img_process/test.py
@@ -4,23 +4,24 @@ import sys
 
 if __name__ == '__main__':
     input_str = " !\"#$%&'()*+'-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
-    # input_str3 = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
-    # img_path = 'naruto.jpg'
-    # output_img = 'naruto_bw3.jpg'
-    # it = ImageTransform.ImageTransform(img_path,char_list=input_str3)
-    # it.img_to_ascii_img(1 / 8, output_img)
-
-    gif_path = 'weisuoyuwei.gif'
     parent_dir = os.path.dirname(sys.path[0])
+    example_dir = os.path.join(os.path.dirname(parent_dir), 'example')
+
+    """
+    JPG transformation
+    """
+    # img_path = os.path.join(example_dir, 'spring.jpg')
+    # output_img_path = os.path.join(example_dir, 'spring_ascii.jpg')
+    # it = ImageTransform.ImageTransform(img_path, char_list=input_str)
+    # it.img_to_ascii_img(1 / 8, output_img_path)
+
+    """
+    GIF transformation
+    """
+    gif_path = os.path.join(example_dir, 'cat.gif')
     png_path = os.path.join(parent_dir, 'tmp')
     if not os.path.exists(png_path):
         os.mkdir(png_path)
+    out_gif_path = os.path.join(example_dir, 'cat_ascii_color.gif')
     it_gif = ImageTransform.ImageTransform(gif_path)
-    it_gif.gif_to_ascii_gif(1/4, 'weisuoyuwei_ascii4.gif', png_path)
-
-    # input = " !\"#$%&'()*+'-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
-    # input2 = "A"
-    # gif_path = 'weisuoyuwei.gif'
-    # it_gif = ImageTransform.ImageTransform(gif_path)
-    # string = it_gif.sort_char_list_by_darkness(input)
-    # print(string)
+    it_gif.gif_to_ascii_gif(1/4, out_gif_path, png_path, color=True)
